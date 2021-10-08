429e98b9e7756a22c4b9672f55954ff7a7605a51
Hoàng <65218576+hoangdev0@users.noreply.github.com>
2021-10-08 11:00:53 +0700

    Fix nguyên tố

--- code1/baitap/Bai_Tap_so_hoc1/analyse.cpp
+++ code1/baitap/Bai_Tap_so_hoc1/analyse.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool k(long long n){
-    for(long long i = 2; i<= sqrt(n);i++)
+    for(long long i = 2; i*i<= n;i++)
         if (n%i == 0) return false;
     return true;
 }
