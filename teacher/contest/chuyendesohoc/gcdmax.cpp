45eae70c93154f2e163dc8afcea9591505bc5b69
hoangdev0 <huyhoang190605@gmail.com>
2021-10-10 23:51:50 +0700

    gcdmax

---
+++ teacher/contest/chuyendesohoc/gcdmax.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+// #define N 1000005
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+#define For(value, begin, end, up) for (int value = begin; value <= end; value += up)
+#define elf else if
+typedef unsigned long long ll;
+using namespace std;
+int main()
+{
+	fr("gcdmax.inp");
+	fw("gcdmax.out");
+	int n,x;
+	cin >> n;
+	For(i,1,n,1)
+	{
+		cin >> x;
+		cout << x/2 << endl;
+	}
+	return 0;
+}
