ee9d53d156f2e41a9937423f6d9e4bf6e9fdd549
hoangdev0 <huyhoang190605@gmail.com>
2021-10-13 23:32:31 +0700

    sohoc2

---
+++ teacher/contest/sohoc2/dayucln.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define N 100005
+#define fr(name) freopen(name, "r", stdin);
+#define fw(name) freopen(name, "w", stdout);
+#define For(value, begin, end, up) for (int value = begin; value <= end; value += up)
+#define elf else if
+typedef unsigned long long ll;
+using namespace std;
+int a[N],b[N],n,x,s;
+int lcm(int a, int b)
+{
+	return a * b / __gcd(a, b);
+}
+int main()
+{
+	fr("dayucln.inp");
+	fw("dayucln.out");
+
+	cin >> n;
+	For(i,0,n-1,1) cin >> a[i];
+	b[0] = a[0];
+	b[n] = a[n-1];
+	For(i,1,n-1,1)
+	{
+		b[i] = lcm(a[i], a[i - 1]);
+	}
+	For(i, 0, n, 1) cout << b[i] << " ";
+	return 0;
+}
